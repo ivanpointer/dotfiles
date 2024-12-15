959f6d4476113a406470726afc355eb59e8bf166
Ivan Pointer <ipointer@Ivans-MacBook-Pro.local>
2024-12-15 13:48:01 -0600

    Poo

--- nvim/.config/nvim/lua/nullpointerexpert/packer.lua
+++ nvim/.config/nvim/lua/nullpointerexpert/packer.lua
@@ -144,4 +144,10 @@ return require('packer').startup(function(use)
     use {
         'christoomey/vim-tmux-navigator'
     }
+
+    -- Automatically set up your configuration after cloning packer.nvim
+    -- Put this at the end after all plugins
+    if packer_bootstrap then
+        require('packer').sync()
+    end
 end)
