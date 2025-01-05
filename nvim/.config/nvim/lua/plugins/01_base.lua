eb00badb8343e3bb7496ad0602c747c2ad67e891
Ivan Pointer <ipointer@Ivans-MacBook-Pro.local>
2025-01-05 10:11:40 -0600

    Add tmux integration for nvim.

--- nvim/.config/nvim/lua/plugins/01_base.lua
+++ nvim/.config/nvim/lua/plugins/01_base.lua
@@ -325,4 +325,24 @@ return {
     -- use opts = {} for passing setup options
     -- this is equivalent to setup({}) function
   },
+
+  {
+    "christoomey/vim-tmux-navigator",
+    lazy = false,
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+      "TmuxNavigatorProcessList",
+    },
+    keys = {
+      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+    },
+  },
 }
