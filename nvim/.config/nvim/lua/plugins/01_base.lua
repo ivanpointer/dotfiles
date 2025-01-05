aee716bacd5eb4832aff5e93d4f6c360ef2493e4
Ivan Pointer <ipointer@Ivans-MacBook-Pro.local>
2025-01-05 13:25:48 -0600

    Use harpoon builtin ui instead of telescope.

--- nvim/.config/nvim/lua/plugins/01_base.lua
+++ nvim/.config/nvim/lua/plugins/01_base.lua
@@ -91,31 +91,15 @@ return {
       "nvim-lua/plenary.nvim",
       "nvim-telescope/telescope.nvim",
     },
-    config = function()
-      local harpoon = require("harpoon")
-
-      local conf = require("telescope.config").values
-      local function toggle_telescope(harpoon_files)
-          local file_paths = {}
-          for _, item in ipairs(harpoon_files.items) do
-              table.insert(file_paths, item.value)
-          end
-
-          require("telescope.pickers").new({}, {
-              prompt_title = "Harpoon",
-              finder = require("telescope.finders").new_table({
-                  results = file_paths,
-              }),
-              previewer = conf.file_previewer({}),
-              sorter = conf.generic_sorter({}),
-          }):find()
-      end
-
-      vim.keymap.set("n", "<leader>ha", function() toggle_telescope(harpoon:list()) end,
-        { desc = "Open harpoon window" })
-
-    end,
     keys = {
+      {
+        "<leader>he",
+        function()
+          local harpoon = require("harpoon")
+          harpoon.ui:toggle_quick_menu(harpoon:list())
+        end,
+        desc = "harpoon file",
+      },
       {
         "<leader>hA",
         function()
